f3e1afe5de19cdb4dd7a630e49f5a560dc6f0c67
Rahulmalhotra10 <86664294+Rahulmalhotra10@users.noreply.github.com>
2026-07-30 16:20:31 +0530

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -137,6 +137,13 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xui6o2l23b");
+</script>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-3390M0M0ER"></script>
 <script dangerouslySetInnerHTML={{ __html: `
   window.dataLayer = window.dataLayer || [];
